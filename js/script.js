00c8a5eca382416fdf97b6270909dd56674df45c
FedeAle1525 <fedeale.1525@gmail.com>
2023-02-09 16:35:08 +0100

    JS code to create cells grid with string

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// 1. Recuperare elemento Pulsante Play da DOM
+const playBtnEl = document.querySelector('button');
+
+// 2. Recupero elemento Griglia da DOM in cui poi inserisco le celle create
+const grigliaEl = document.querySelector('.griglia');
+
+// 3. Aggancio evento "click" al Pulsante Play recuperato da DOM
+playBtnEl.addEventListener('click', function(){
+  
+  // 4. Imposto Dimensione delle Celle e Numero complessivo di Celle [Gliglia Quadrata 10x10]  
+  latoGriglia = 10;
+  numCelle = latoGriglia * latoGriglia;
+
+  // 5. Creo Ciclo di 'numCelle' interazione ed a ogni interazione creo una Cella da inserire dentro 'GrigliaEl'
+  for (let i=0; i < numCelle; i++){
+
+    // 5.1 - Creo Stringa che rappresenta la Cella
+    const cellaStringa = `<div>${i+1}</div>`;
+    
+    // 5.2 - Inserisco la Cella creata dentro la Griglia nel DOM
+    grigliaEl.innerHTML += cellaStringa;
+
+  }
+
+  // 6 - Prendo dal DOM tutte le Celle della Griglia
+  const listaCelle = document.querySelectorAll('.griglia > div');
+  console.log(listaCelle);
+
+  // 6. Creo Ciclo per aggangiare a ogni cella evento "click"
+  for (let i=0; i < numCelle; i++){
+
+    // 6.1 - Al "click" sulla cella appare un messaggio nella console con il numero della cella
+    listaCelle[i].addEventListener('click', function(){
+      console.log(`Questa e' la cella numnero ${i+1}`);
+    });
+
+  }
+
+});
